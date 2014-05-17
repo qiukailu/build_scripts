d356f188fbba6a99b05f027685c486c6441ccad2
qiukailu <qiukailu@users.noreply.github.com>
2014-05-16 23:50:05 -0400

    added nightly build script for CCI BG/Q

---
+++ nightly_build_cci_bgq.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -ex
+
+LOCAL_DIR=weekly_build;
+BUILD_DIR=weekly_build;
+
+rm -rf weekly_build.tar.gz;
+rm -rf $LOCAL_DIR; 
+mkdir $LOCAL_DIR;
+cd $LOCAL_DIR;
+
+svn co http://redmine.scorec.rpi.edu/svn/slac/trunk/Scripts Scripts;
+ln -s Scripts/checkout/checkout_source_code.sh .;
+./checkout_source_code.sh;
+
+# pack up things
+cd ..
+tar zcf weekly_build.tar.gz weekly_build;
+
+# send to CCNI
+scp weekly_build.tar.gz $CCNI:~/
+
+# go to CCNI BG/Q
+
+ssh $CCNI ' ssh q ' ./WeeklyBuild.sh ' ';
